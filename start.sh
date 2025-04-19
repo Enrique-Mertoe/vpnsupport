68adde5873f6473180a611d51a7ec4f47e81d016
abuti <abutimartin778@gmail.com>
2025-04-19 09:01:28 +0300

    c1

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 # Wait for Redis to be ready
 echo "Waiting for Redis to be ready..."
-while ! redis-cli -h redis ping > /dev/null 2>&1; do
-    echo "Redis is not ready yet. Retrying in 5 seconds..."
-    sleep 5
-done
+#while ! redis-cli -h redis ping > /dev/null 2>&1; do
+#    echo "Redis is not ready yet. Retrying in 5 seconds..."
+#    sleep 5
+#done
 echo "Redis is ready!"
 
 # Start Gunicorn
